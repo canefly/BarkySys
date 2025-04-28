bf9556bf6d88e25557ddb4e67d74b801b102a3be
canefly <canefly@outlook.ph>
2025-04-28 13:57:27 +0800

    Update query to fetch dog grooming services instead of haircut services

--- components/dog-grooming.php
+++ components/dog-grooming.php
@@ -7,7 +7,7 @@ include 'db.php'; // Include database connection
     <h2 style="color: #6E3387; text-align: center; font-weight: 600;">Dog Grooming Services</h2>
     <div class="services">
         <?php
-        $query = "SELECT * FROM services WHERE service_type = 'Haircut'";
+        $query = "SELECT * FROM services WHERE service_type = 'DogGrooming'";
         $result = mysqli_query($conn, $query);
 
         if (mysqli_num_rows($result) > 0) {
